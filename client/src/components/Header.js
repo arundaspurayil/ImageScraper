5b98757275093c870dd7669b56556db4e6017d41
Arundas Purayil <arundas0purayil@gmail.com>
2020-05-11 15:31:05 -0400

    Added links to icons

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,35 +1,41 @@
-import React from "react";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-} from "@material-ui/core";
+import React from 'react'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
 
-import { makeStyles } from "@material-ui/styles";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import { makeStyles } from '@material-ui/styles'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
 
 const useStyles = makeStyles(() => ({
-  typographyStyles: {
-    flex: 1
-  },
-  iconStyles: {
-    padding: "5px"
-  }
-}));
+    typographyStyles: {
+        flex: 1,
+    },
+    iconStyles: {
+        padding: '5px',
+    },
+}))
 function Header() {
-  const classes = useStyles();
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography className={classes.typographyStyles}>
-          Arundas Purayil
-        </Typography>
-        <LinkedInIcon className={classes.iconStyles} />
-        <GitHubIcon className={classes.iconStyles} />
-      </Toolbar>
-    </AppBar>
-  );
+    const classes = useStyles()
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography className={classes.typographyStyles}>
+                    Arundas Purayil
+                </Typography>
+                <a
+                    style={{ color: 'inherit' }}
+                    href="https://www.linkedin.com/in/arundas-purayil/"
+                >
+                    <LinkedInIcon className={classes.iconStyles} />
+                </a>
+                <a
+                    style={{ color: 'inherit' }}
+                    href="https://github.com/arundaspurayil"
+                >
+                    <GitHubIcon className={classes.iconStyles} />
+                </a>
+            </Toolbar>
+        </AppBar>
+    )
 }
 
-export default Header;
+export default Header
