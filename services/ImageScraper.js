bb4c0ad4fca5e96b0ba0c74eec6de711e623e0b4
Arundas Purayil <arundas0purayil@gmail.com>
2020-05-13 19:01:14 -0400

    Fixed Puppeteer bug

--- services/ImageScraper.js
+++ services/ImageScraper.js
@@ -32,7 +32,9 @@ async function visitSubPages(page, links) {
 async function getAllImages(url) {
     let images = []
 
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
     const page = await browser.newPage()
 
     await page.setRequestInterception(true)
