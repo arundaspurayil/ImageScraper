4f6949491a20803719553142b628f5bc880882a0
Arundas Purayil <arundas0purayil@gmail.com>
2020-05-13 18:56:00 -0400

    Heroku Takeoff

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use('/api', apiRouter)
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client', 'build')))
-    app.get('/*', function (req, res) {
+    app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
