e812b7431780b104ee6c83dbfdf06117f42f7ee0
Arundas Purayil <arundas0purayil@gmail.com>
2020-05-13 19:06:25 -0400

    Heroku fix

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use('/api', apiRouter)
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client', 'build')))
+
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
