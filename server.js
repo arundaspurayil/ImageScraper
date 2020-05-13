035af91a31b066ea0879dfb142c885e16ca5062f
Arundas Purayil <arundas0purayil@gmail.com>
2020-05-13 18:19:54 -0400

    Changed configuration to push to Heroku

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const path = require('path')
 const apiRouter = require('./routes/api')
+
+const PORT = process.env.PORT || '5000'
 
 app.use('/api', apiRouter)
 
-const port = process.env.PORT || '5000'
-app.listen(port, (err) => {
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client', 'build')))
+    app.get('/*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+app.listen(PORT, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
-    console.log(`server is listening on ${port}`)
+    console.log(`server is listening on ${PORT}`)
 })
