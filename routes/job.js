956630c5ca6421e0f91adc4926d58ae85a082440
Arundas Purayil <arundas0purayil@gmail.com>
2020-05-17 15:56:03 -0400

    Added route to check job id's for image downloads

--- routes/job.js
+++ routes/job.js
@@ -4,7 +4,7 @@ const router = express.Router()
 let Queue = require('bull')
 const opts = require('../worker_redis')
 
-router.get('/:id', async (req, res) => {
+router.get('/scrape/:id', async (req, res) => {
     let scraperQueue = new Queue('scraper', opts)
     const id = req.params.id
     let job = await scraperQueue.getJob(id)
@@ -17,6 +17,24 @@ router.get('/:id', async (req, res) => {
     if (job.returnvalue) {
         let { images } = job.returnvalue
         res.json({ state: state, images: images })
+    } else {
+        res.json({ state: state })
+    }
+})
+
+router.get('/download/:id', async (req, res) => {
+    let downloadQueue = new Queue('download', opts)
+    const id = req.params.id
+    let job = await downloadQueue.getJob(id)
+
+    if (job === null) {
+        return res.status(404).end()
+    }
+
+    let state = await job.getState()
+    if (job.returnvalue) {
+        let { downloadUrl } = job.returnvalue
+        res.json({ state: state, downloadUrl: downloadUrl })
     } else {
         res.json({ state: state })
     }
