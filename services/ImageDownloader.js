ff669e0132d53459930541187bbc0842f331bdf9
Arundas Purayil <arundas0purayil@gmail.com>
2020-05-17 15:55:36 -0400

    Change ImageDownlaoder function to return promise containing file path to zip file

--- services/ImageDownloader.js
+++ services/ImageDownloader.js
@@ -1,43 +1,51 @@
 const axios = require('axios')
 const archiver = require('archiver')
+const fs = require('fs')
 
-async function downloadImages(output, images) {
-    let archive = archiver('zip', {
-        zlib: { level: 9 }, // Sets the compression level.
-    })
+async function downloadImages(images) {
+    return new Promise(async (resolve, reject) => {
+        let output = fs.createWriteStream(__dirname + '/example.zip')
 
-    output.on('end', function () {
-        console.log('Data has been drained')
-    })
-    archive.on('warning', function (err) {
-        if (err.code === 'ENOENT') {
-            // log warning
-        } else {
-            // throw error
-            throw err
-        }
-    })
-
-    // good practice to catch this error explicitly
-    archive.on('error', function (err) {
-        throw err
-    })
-
-    archive.pipe(output)
-    while (images.length > 0) {
-        let image = images.pop()
-        const fileName = image.split('/').pop()
-        const response = await axios.get(image, {
-            validateStatus: false,
-            responseType: 'arraybuffer',
+        let archive = archiver('zip', {
+            zlib: { level: 9 }, // Sets the compression level.
         })
-        if (response.status == 200) {
-            const buffer = Buffer.from(response.data, 'utf-8')
-            archive.append(buffer, { name: fileName })
-        }
-    }
 
-    archive.finalize()
+        output.on('end', function () {
+            console.log('Data has been drained')
+        })
+        archive.on('warning', function (err) {
+            if (err.code === 'ENOENT') {
+                console.log(err)
+            } else {
+                // throw error
+                reject(err)
+            }
+        })
+
+        archive.on('error', function (err) {
+            reject(err)
+        })
+
+        archive.pipe(output)
+        while (images.length > 0) {
+            let image = images.pop()
+            const fileName = image.split('/').pop()
+            const response = await axios.get(image, {
+                validateStatus: false,
+                responseType: 'arraybuffer',
+            })
+            if (response.status == 200) {
+                const buffer = Buffer.from(response.data, 'utf-8')
+                archive.append(buffer, { name: fileName })
+            }
+        }
+
+        archive.finalize()
+
+        output.on('close', function () {
+            resolve(__dirname + '/example.zip')
+        })
+    })
 }
 
 module.exports = downloadImages
