12a002cd3e7c933a0402f1c0c911f87694fa6b84
Arundas Purayil <arundas0purayil@gmail.com>
2020-05-17 15:55:11 -0400

    Added new queue to download images as zip and upload to S3

--- worker.js
+++ worker.js
@@ -4,6 +4,8 @@ let Queue = require('bull')
 const opts = require('./worker_redis')
 
 const getAllImages = require('./services/ImageScraper')
+const downloadImages = require('./services/ImageDownloader')
+const upload = require('./services/UploadToS3')
 
 let workers = process.env.WEB_CONCURRENCY || 1
 
@@ -11,11 +13,20 @@ let maxJobsPerWorker = 50
 
 function start() {
     let scraperQueue = new Queue('scraper', opts)
+    let downloadQueue = new Queue('download', opts)
 
     scraperQueue.process(maxJobsPerWorker, async (job) => {
         const url = job.data.url
         const links = await getAllImages(url)
         return { images: links }
+    })
+
+    downloadQueue.process(maxJobsPerWorker, async (job) => {
+        const images = job.data.images
+        const zipPath = await downloadImages(images)
+        const awsUrl = await upload(zipPath)
+
+        return { downloadUrl: awsUrl }
     })
 }
 
